d31709de8cfd781b66a48c6da63aa247ca0c849c
Brener <brener@bizut.com.br>
2022-08-07 21:41:17 -0300

    set headers cors

--- backend/mycontacts/src/index.js
+++ backend/mycontacts/src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const routes = require('./routes');
 require('express-async-errors');
+
+const routes = require('./routes');
+const cors = require('./middlewares/cors');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.error(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(3001, () => console.log('Server started at http://localhost:3001'));
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,47 +1,66 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import {
-  Container, Header, ListContainer, Card, InputSearchContainer,
+  Container, Header, ListHeader, Card, InputSearchContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then(async (res) => {
+        const json = await res.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+        console.error('error::', error);
+      });
+  }, []);
+
+  function handleToggleOrderBy() { }
+
   return (
     <Container>
-      <Modal danger />
-
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato" />
       </InputSearchContainer>
 
       <Header>
-        <strong>Contacts list</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
-          </button>
-        </header>
+      <ListHeader>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span>Nome</span>
+          <img src={arrow} alt="Arrow" />
+        </button>
+      </ListHeader>
 
-        <Card>
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
-              <strong>Brener Sousa</strong>
-              <small>instagram</small>
+              <strong>{contact.name}</strong>
+              {contact.category_name && (
+                <small>{contact.category_name}</small>
+              )}
             </div>
-            <span>brener.dev@gmail.com</span>
-            <span>(21) 99999-9999</span>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
           </div>
           <div className="actions">
-            <Link to="/edit/123">
+            <Link to={`/edit/${contact.id}`}>
               <img src={edit} alt="Edit" />
             </Link>
             <button type="button">
@@ -49,8 +68,7 @@ export default function Home() {
             </button>
           </div>
         </Card>
-
-      </ListContainer>
+      ))}
     </Container>
   );
 }
