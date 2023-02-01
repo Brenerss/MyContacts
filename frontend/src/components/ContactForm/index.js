eeab8d7a7711a1a05a59766bbbb041d30ff487d0
Brener Sousa <brenersousarj@outlook.com>
2023-02-01 01:17:27 -0300

    feat: edit contact page and clear fields after insert neew contact

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import {
+  forwardRef, useEffect, useState, useImperativeHandle,
+} from 'react';
 import PropTypes from 'prop-types';
 
 import isEmailValid from '../../utils/isEmailValid';
@@ -14,7 +16,8 @@ import useErrors from '../../hooks/useErrors';
 import formatPhone from '../../utils/formatPhone';
 import CategoriesService from '../../services/CategoriesService';
 
-export default function ContactForm({ buttonLabel, onSubmit }) {
+const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
+  console.log(ref);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -81,6 +84,22 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     loadCategories();
   }, []);
 
+  useImperativeHandle(ref, () => ({
+    setFieldValues: (contact) => {
+      setName(contact.name ?? '');
+      setEmail(contact.email ?? '');
+      setPhone(formatPhone(contact.phone) ?? '');
+      setCategoryId(contact.category_id ?? '');
+    },
+
+    resetFields: () => {
+      setName('');
+      setEmail('');
+      setPhone('');
+      setCategoryId('');
+    },
+  }), []);
+
   return (
     <Form onSubmit={handleSubmit} noValidate>
       <FormGroup error={getErrorMessageByFieldMessage('name')}>
@@ -140,9 +159,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
       </ButtonContainer>
     </Form>
   );
-}
+});
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,11 +1,81 @@
+import { useHistory, useParams } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+
+import ContactService from '../../services/ContactService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const { id } = useParams();
+  const history = useHistory();
+  const contactFormRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  useEffect(() => {
+    async function loadContacts() {
+      try {
+        const contact = await ContactService.getContactById(id);
+        contactFormRef.current.setFieldValues(contact);
+        setContactName(contact.name);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato nao encontrado!',
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadContacts();
+  }, [id, history]);
+
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const updatedContact = await ContactService.updateContact(
+        id,
+        contact,
+      );
+
+      setContactName(updatedContact.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch (error) {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+
   return (
     <>
-      <PageHeader title="Editar Brener Sousa" />
-      <ContactForm buttonLabel="Salvar Alterações" />
+      <Loader isLoading={isLoading} />
+
+      <PageHeader
+        title={isLoading ? 'Carregando...' : `Editar ${contactName}`}
+      />
+
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar Alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
